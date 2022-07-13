02c76ebc31497eb52ae1aba9f74d1b13ac353bfc
凱恩Kane <50613gary@gmail.com>
2022-07-13 13:42:57 +0800

    fix links

--- src/variables/links.js
+++ src/variables/links.js
@@ -1,10 +1,15 @@
-export const oauth2 =
-  "https://discord.com/api/oauth2/authorize?client_id=993728322141962292&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=token&scope=guilds%20identify";
+const isProduction = process.env.NODE_ENV === "production"
 
-export const support = "https://discord.com/invite/RKmxpBcTMs"
+const redirectUri = isProduction ? "https://app.yeecord.com" : "http://localhost:3000"
+const clientId = isProduction ? "584213384409382953" : "840770984821784601"
+
+export const oauth2 =
+  `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}/auth&response_type=token&scope=guilds%20identify`
+
+export const support = "https://discord.gg/yeecord"
 
 export const homepage = "https://yeecord.com/"
 
 export const blog = "https://yeecord.com/blog"
 
-export const invite = "https://discord.com/oauth2/authorize?client_id=584213384409382953&permissions=277850745905&scope=bot%20applications.commands"
+export const invite = "https://yeecord.com/link"
