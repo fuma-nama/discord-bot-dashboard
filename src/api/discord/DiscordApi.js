5c2201bd0c4df024228a7d622f9afcb500c58256
凱恩Kane <50613gary@gmail.com>
2022-07-14 00:31:48 +0800

    test guilds

--- src/api/discord/DiscordApi.js
+++ src/api/discord/DiscordApi.js
@@ -1,30 +1,19 @@
 import {fetchAuto} from "../utils";
+import {api} from "../../variables/links";
 
 export async function getAccountInfo() {
   const user = await fetchAuto(`/users/@me`, {toJson: true})
 
-  /*
   const guilds = await fetch(
-      "https://discordapp.com/api/users/@me/guilds",
+      `${api}/guilds`,
       {
-        headers: {
-          authorization: `${tokenType} ${accessToken}`,
-        },
+        credentials: "include"
       }
   );
-   */
 
   return {
     user,
-    guilds: [
-      {
-        "icon": "d7438912f61e7cf6c9178f2c32691631",
-        "owner": true,
-        id: "600363644991176822",
-        name: "Kanecord Cosplay Community",
-      }
-    ]
-    //guilds: Array.from(await guilds.json()).filter((g) => g.owner),
+    guilds: await guilds.json()
   };
 }
 
