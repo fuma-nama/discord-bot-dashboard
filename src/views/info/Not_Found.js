a491a80e9cfc1df8436a6ddeab7d49134e6f7224
SonMooSans <god63820869@gmail.com>
2022-08-11 19:42:19 +0800

    Add Not Found Page

---
+++ src/views/info/Not_Found.js
@@ -0,0 +1,12 @@
+import {Box, Center, Heading, Image, Text} from "@chakra-ui/react";
+import not_found from "assets/img/info/not_found.svg"
+
+export default function NotFound() {
+    return <Center h="100vh" position="relative">
+        <Box position="relative">
+            <Image width={400} src={not_found} alt="Not Found" />
+            <Heading>404 Not Found</Heading>
+            <Text fontSize={20}>我們找不到您請求的資源</Text>
+        </Box>
+    </Center>
+}
