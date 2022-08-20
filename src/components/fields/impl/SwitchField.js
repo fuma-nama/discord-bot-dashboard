5a8c40aea32e4773d883a073db5e4c51adf36648
SonMooSans <god63820869@gmail.com>
2022-08-20 18:49:09 +0800

    Improve Switch Field

--- src/components/fields/impl/SwitchField.js
+++ src/components/fields/impl/SwitchField.js
@@ -1,12 +1,5 @@
 // Chakra imports
-import {
-  Box,
-  Flex,
-  FormLabel,
-  Switch,
-  Text,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import {Box, Flex, FormLabel, Switch, Text, useColorModeValue,} from "@chakra-ui/react";
 // Custom components
 import React from "react";
 
@@ -16,60 +9,22 @@ export default function Default(props) {
     label,
     isChecked,
     onChange,
-    desc,
-    textWidth,
     reversed,
-    fontSize,
     ...rest
   } = props;
   const textColorPrimary = useColorModeValue("secondaryGray.900", "white");
   return (
     <Box w="100%" fontWeight="500" {...rest}>
-      {reversed ? (
-        <Flex align="center" borderRadius="16px">
-          <Switch
-            isChecked={isChecked}
-            id={id}
-            variant="main"
-            colorScheme="brandScheme"
-            size="md"
-            onChange={onChange}
-          />
+        <Flex direction={reversed? "row-reverse" : "row"} align="center" justify={reversed? "start" : "space-between"} gap={5}>
           <FormLabel
-            ms="15px"
-            htmlFor={id}
-            _hover={{ cursor: "pointer" }}
-            direction="column"
-            mb="0px"
-            maxW={textWidth ? textWidth : "75%"}
+              htmlFor={id}
+              _hover={{ cursor: "pointer" }}
+              direction="column"
+              mb="0px"
+              maxW="75%"
           >
-            <Text color={textColorPrimary} fontSize="md" fontWeight="500">
+            <Text align="start" color={textColorPrimary} fontSize="md" fontWeight="500">
               {label}
-            </Text>
-            <Text
-              color="secondaryGray.600"
-              fontSize={fontSize ? fontSize : "md"}
-            >
-              {desc}
-            </Text>
-          </FormLabel>
-        </Flex>
-      ) : (
-        <Flex justify="space-between" align="center" borderRadius="16px">
-          <FormLabel
-            htmlFor={id}
-            _hover={{ cursor: "pointer" }}
-            direction="column"
-            maxW={textWidth ? textWidth : "75%"}
-          >
-            <Text color={textColorPrimary} fontSize="md" fontWeight="500">
-              {label}
-            </Text>
-            <Text
-              color="secondaryGray.600"
-              fontSize={fontSize ? fontSize : "md"}
-            >
-              {desc}
             </Text>
           </FormLabel>
           <Switch
@@ -81,7 +36,6 @@ export default function Default(props) {
             onChange={onChange}
           />
         </Flex>
-      )}
     </Box>
   );
 }
