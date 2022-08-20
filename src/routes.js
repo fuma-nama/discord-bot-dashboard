a90c312d6b18906966501b0418e2f4d2b19228ab
SonMooSans <god63820869@gmail.com>
2022-08-20 22:21:28 +0800

    Localize Route Names

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-import { Icon } from "@chakra-ui/react";
-import { RiFunctionFill } from "react-icons/ri";
-import { BiServer } from "react-icons/bi";
+import {Icon} from "@chakra-ui/react";
+import {RiFunctionFill} from "react-icons/ri";
+import {BiServer} from "react-icons/bi";
 import {MdFrontHand} from "react-icons/md";
-import { IoIosSettings, IoIosArrowRoundBack } from "react-icons/io";
+import {IoIosArrowRoundBack, IoIosSettings} from "react-icons/io";
 import {Navigate} from "react-router-dom";
 // Admin Imports
 import Dashboard from "views/guild/dashboard";
 import Features from "views/guild/features";
 import SettingsPanel from "./views/guild/settings";
 import ActionsBoard from "./views/guild/action";
+import {Locale} from "./utils/Language";
 
 /**
  * Public Routes that can access on sidebar
@@ -18,43 +19,40 @@ import ActionsBoard from "./views/guild/action";
  */
 const routes = [
   {
-    name: "返回個人資料面板",
+    name: <Locale zh="返回個人資料面板" en="Back to Profile" />,
     path: "../",
     icon: <Icon as={IoIosArrowRoundBack} width="20px" height="20px" color="inherit" />,
     component: <Navigate replace to="/admin" />
   },
   {
-    name: "服務器儀表板",
+    name: <Locale zh="服務器儀表板" en="Statistics" />,
     path: "dashboard",
     icon: <Icon as={BiServer} width="20px" height="20px" color="inherit" />,
     component: <Dashboard />,
   },
   {
-    name: "功能控制板",
+    name: <Locale zh="功能控制板" en="Features" />,
     path: "features",
     icon: (
       <Icon as={RiFunctionFill} width="20px" height="20px" color="inherit" />
     ),
     component: <Features />,
-    secondary: true,
   },
   {
-    name: "動作面板",
+    name: <Locale zh="動作面板" en="Actions" />,
     path: "actions",
     icon: (
         <Icon as={MdFrontHand} width="20px" height="20px" color="inherit" />
     ),
     component: <ActionsBoard />,
-    secondary: true
   },
   {
-    name: "服務器設置",
+    name: <Locale zh="服務器設置" en="Settings" />,
     path: "settings",
     icon: (
         <Icon as={IoIosSettings} width="20px" height="20px" color="inherit" />
     ),
     component: <SettingsPanel />,
-    secondary: true
   },
 ];
 
