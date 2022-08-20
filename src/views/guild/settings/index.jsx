709f6fffc44d532efcc269ff2e27b42d7265c499
SonMooSans <god63820869@gmail.com>
2022-08-20 22:24:42 +0800

    Localize Server Settings

--- src/views/guild/settings/index.jsx
+++ src/views/guild/settings/index.jsx
@@ -1,28 +1,26 @@
 import React, {useContext, useMemo} from "react";
 
-import {Box, Flex} from "@chakra-ui/react";
+import {Box} from "@chakra-ui/react";
 
 import {usePageInfo} from "contexts/PageInfoContext";
 import {SettingsContext, SettingsProvider} from "../../../contexts/guild/GuildSettingsContext";
 import {GuildContext} from "contexts/guild/GuildContext";
 import {ConfigGrid} from "components/fields/ConfigPanel";
 import {updateSettingsOptions} from "api/yeecord";
-import {config} from "../../../config/config";
+import {config} from "config/config";
 import {useQueryClient} from "react-query";
+import {useLocale} from "utils/Language";
 
 export default function SettingsPanel() {
-    usePageInfo("服務器設置")
+    const locale = useLocale()
+    usePageInfo(
+        locale({zh: "服務器設置", en: "Server Settings"})
+    )
 
     return (
         <SettingsProvider>
             <Box pt={{base: "180px", md: "80px", xl: "80px"}}>
-                <Flex
-                    flexDirection="column"
-                    mb="10"
-                    gridArea={{ xl: "1 / 1 / 2 / 3", "2xl": "1 / 1 / 2 / 2" }}
-                >
-                    <SettingsConfigPanel />
-                </Flex>
+                <SettingsConfigPanel />
             </Box>
     </SettingsProvider>
   );
