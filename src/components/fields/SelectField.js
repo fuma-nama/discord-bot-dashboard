61c0c1bca558810a2931fac0b2d0c8a9af2b05f5
SonMooSans <god63820869@gmail.com>
2022-08-20 18:17:55 +0800

    Fix Select Field value bugs

--- src/components/fields/SelectField.js
+++ src/components/fields/SelectField.js
@@ -1,7 +1,12 @@
 import {Select as BaseSelect} from "chakra-react-select";
 import {useColorModeValue} from "@chakra-ui/react";
 
-export function SelectField(props) {
+export function SelectField({value, onChange, options, ...props}) {
+    const selected = value != null && Array.isArray(options) && options.find(option =>
+        option.value === value ||
+        (Array.isArray(value) && value.includes(option.value))
+    )
+
     const brandColor = useColorModeValue("secondaryGray.300", "brand.400");
     const bgColor = useColorModeValue("secondaryGray.300", "navy.900")
 
@@ -19,5 +24,12 @@ export function SelectField(props) {
         focusBorderColor={brandColor}
         selectedOptionStyle="check"
         {...props}
+        value={selected}
+        onChange={e =>
+            onChange != null && onChange(
+                Array.isArray(e)? e.map(option => option.value) : e.value
+            )
+        }
+        options={options}
     />
 }
