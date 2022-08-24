eed00669713685eca19dea740b9ad74474fa3af8
SonMooSans <god63820869@gmail.com>
2022-08-24 17:43:26 +0800

    Fix Sidebar Guild Preview

--- src/components/sidebar/components/Brand.js
+++ src/components/sidebar/components/Brand.js
@@ -1,13 +1,12 @@
 import React, {useContext} from "react";
 
 // Chakra imports
-import {Avatar, Flex, HStack, Text} from "@chakra-ui/react";
+import {Avatar, Flex, HStack, Skeleton, Text} from "@chakra-ui/react";
 
 // Custom components
 import {HSeparator} from "components/separator/Separator";
 import {GuildContext} from "contexts/guild/GuildContext";
 import {iconToUrl, useGuild} from "api/discord/DiscordApi";
-import {QueryHolderSkeleton} from "contexts/components/AsyncContext";
 import {config} from "../../../config/config";
 import {useTextColor} from "../../../utils/colors";
 
@@ -17,7 +16,7 @@ export function SidebarBrand() {
   const {id} = useContext(GuildContext)
 
   return (
-    <Flex align="center" direction="column">
+    <Flex align="center" direction="column" gap={5}>
         {
             id != null? (
                 <GuildHeader id={id} logoColor={logoColor} />
@@ -35,15 +34,15 @@ export function SidebarBrand() {
 function GuildHeader({id, logoColor}) {
   const query = useGuild(id)
 
-  return <QueryHolderSkeleton query={query} height="100px" count={1}>{() =>
-      <HStack align="center" mb={5} px={5}>
+  return query.isLoading?
+      <Skeleton w="80%" h="5rem" rounded="xl" />
+      :
+      <HStack align="center" px={5}>
         <Avatar name={query.data.name} src={iconToUrl(query.data.id, query.data.icon)} />
         <Text fontWeight="extrabold" fontSize="2xl" color={logoColor}>
           {query.data.name}
         </Text>
       </HStack>
-  }
-  </QueryHolderSkeleton>
 }
 
 export default SidebarBrand;
