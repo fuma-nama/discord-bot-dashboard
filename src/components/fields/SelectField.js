5c6e2d3d20a46d90c106c50484d0ef82e68da0ee
SonMooSans <god63820869@gmail.com>
2022-12-03 17:33:50 +0800

    Wrap dropdown menu into portal

--- src/components/fields/SelectField.js
+++ src/components/fields/SelectField.js
@@ -1,35 +1,45 @@
-import {Select as BaseSelect} from "chakra-react-select";
-import {useColorModeValue} from "@chakra-ui/react";
+import { Select as BaseSelect } from "chakra-react-select";
+import { useColorModeValue } from "@chakra-ui/react";
 
-export function SelectField({value, onChange, options, ...props}) {
-    const selected = value != null && Array.isArray(options) && options.find(option =>
+export function SelectField({ value, onChange, options, ...props }) {
+  const selected =
+    value != null &&
+    Array.isArray(options) &&
+    options.find(
+      (option) =>
         option.value === value ||
         (Array.isArray(value) && value.includes(option.value))
-    )
+    );
 
-    const brandColor = useColorModeValue("secondaryGray.300", "brand.400");
-    const bgColor = useColorModeValue("secondaryGray.300", "navy.900")
+  const brandColor = useColorModeValue("secondaryGray.300", "brand.400");
+  const bgColor = useColorModeValue("secondaryGray.300", "navy.900");
 
-    return <BaseSelect
-        chakraStyles={{
-            dropdownIndicator: (provided) => ({
-                ...provided,
-               background: "brand",
-            }),
-            menuList: (provided) => ({
-                ...provided,
-                background: bgColor,
-            })
-        }}
-        focusBorderColor={brandColor}
-        selectedOptionStyle="check"
-        {...props}
-        value={selected}
-        onChange={e =>
-            onChange != null && onChange(
-                Array.isArray(e)? e.map(option => option.value) : e.value
-            )
-        }
-        options={options}
+  return (
+    <BaseSelect
+      menuPortalTarget={document.body}
+      styles={{
+        menuPortal: (provided) => ({ ...provided, zIndex: 100 }),
+      }}
+      chakraStyles={{
+        dropdownIndicator: (provided) => ({
+          ...provided,
+          background: "brand",
+        }),
+        menuList: (provided) => ({
+          ...provided,
+          background: bgColor,
+          zIndex: 11,
+        }),
+      }}
+      focusBorderColor={brandColor}
+      selectedOptionStyle="check"
+      {...props}
+      value={selected}
+      onChange={(e) =>
+        onChange != null &&
+        onChange(Array.isArray(e) ? e.map((option) => option.value) : e.value)
+      }
+      options={options}
     />
+  );
 }
